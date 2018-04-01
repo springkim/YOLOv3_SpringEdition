30001a5490484bef5327e91c40932b3dc5265373
springkim <springnode@gmail.com>
2018-04-01 22:05:13 +0900

    support linux

---
+++ build/CMakeFiles/YOLOv2SE.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/activation_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/activation_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/activations.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/activations.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/avgpool_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/avgpool_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/batchnorm_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/batchnorm_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/blas.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/blas.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/box.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/box.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/col2im.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/col2im.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/connected_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/connected_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/convolutional_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/convolutional_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/cost_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/cost_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/crnn_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/crnn_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/crop_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/crop_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/cuda.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/cuda.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/data.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/data.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/deconvolutional_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/deconvolutional_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/demo.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/demo.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/detection_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/detection_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/detector.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/detector.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/dropout_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/dropout_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/gemm.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/gemm.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/gru_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/gru_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/im2col.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/im2col.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/image.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/image.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/list.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/list.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/local_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/local_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/lstm_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/lstm_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/main.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/main.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/matrix.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/matrix.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/maxpool_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/maxpool_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/network.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/network.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/normalization_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/normalization_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/option_list.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/option_list.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/parser.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/parser.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/region_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/region_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/reorg_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/reorg_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/rnn_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/rnn_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/route_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/route_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/shortcut_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/shortcut_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/softmax_layer.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/softmax_layer.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/tree.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/tree.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/utils.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/utils.c.o"
+  "/home/spring/문서/git/YOLOv2_SpringEdition/src/yolo_v2.c" "/home/spring/문서/git/YOLOv2_SpringEdition/build/CMakeFiles/YOLOv2SE.dir/src/yolo_v2.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CUDNN"
+  "GPU"
+  "_TIMESPEC_DEFINED"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/cuda-8.0/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
