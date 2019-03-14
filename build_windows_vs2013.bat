d76521d8c98d473f77e7d5f3b4c98fc77163c508
springkim <springnode@gmail.com>
2019-03-14 17:39:15 +0900

    support cuda 10 and cudnn 7.5

--- build_windows_vs2013.bat
+++ build_windows_vs2013.bat
@@ -1,33 +1,29 @@
 @echo off
+mkdir -p build\Release
 nvcc -V | findstr /C:"release 8.0" > cuda_ver.txt
 set /p "ver80="<"cuda_ver.txt"
 if not "%ver80%" equ "" (
 	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda8.0/cudnn.h','3rdparty\include\cudnn.h')"
 	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda8.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda8.0/cudnn64_7.dll','build\Release\cudnn64_7.dll')"
 )
 nvcc -V | findstr /C:"release 9.0" > cuda_ver.txt
 set /p "ver90="<"cuda_ver.txt"
 if not "%ver90%" equ "" (
 	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda9.0/cudnn.h','3rdparty\include\cudnn.h')"
 	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda9.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda9.0/cudnn64_7.dll','build\Release\cudnn64_7.dll')"
 )
 nvcc -V | findstr /C:"release 10.0" > cuda_ver.txt
 set /p "ver100="<"cuda_ver.txt"
 if not "%ver100%" equ "" (
-	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.3.1_cuda10.0/cudnn.h','3rdparty\include\cudnn.h')"
-	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.3.1_cuda10.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.5cuda10.0/cudnn.h','3rdparty\include\cudnn.h')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.5cuda10.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.5cuda10.0/cudnn64_7.dll','build\Release\cudnn64_7.dll')"
 )
 DEL cuda_ver.txt
 md build
 cd build
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\MSBuild.exe" (
-	cmake .. -G "Visual Studio 12 2013 Win64"
-)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\MSBuild.exe" (
-	cmake .. -G "Visual Studio 14 2015 Win64"
-)
-if exist 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MSBuild.exe' (
-	cmake .. -G "Visual Studio 15 2017 Win64"
-)
+cmake .. -G "Visual Studio 12 2013 Win64"
 cmake --build . --config Release --target ALL_BUILD
 pause
---
+++ build_windows_vs2017.bat
@@ -0,0 +1,29 @@
+@echo off
+mkdir -p build\Release
+nvcc -V | findstr /C:"release 8.0" > cuda_ver.txt
+set /p "ver80="<"cuda_ver.txt"
+if not "%ver80%" equ "" (
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda8.0/cudnn.h','3rdparty\include\cudnn.h')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda8.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda8.0/cudnn64_7.dll','build\Release\cudnn64_7.dll')"
+)
+nvcc -V | findstr /C:"release 9.0" > cuda_ver.txt
+set /p "ver90="<"cuda_ver.txt"
+if not "%ver90%" equ "" (
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda9.0/cudnn.h','3rdparty\include\cudnn.h')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda9.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.1cuda9.0/cudnn64_7.dll','build\Release\cudnn64_7.dll')"
+)
+nvcc -V | findstr /C:"release 10.0" > cuda_ver.txt
+set /p "ver100="<"cuda_ver.txt"
+if not "%ver100%" equ "" (
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.5cuda10.0/cudnn.h','3rdparty\include\cudnn.h')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.5cuda10.0/cudnn.lib','3rdparty\lib\cudnn.lib')"
+	powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/springkim/YOLOv3_SpringEdition/releases/download/cudnn7.5cuda10.0/cudnn64_7.dll','build\Release\cudnn64_7.dll')"
+)
+DEL cuda_ver.txt
+md build
+cd build
+cmake .. -G "Visual Studio 15 2017 Win64"
+cmake --build . --config Release --target ALL_BUILD
+pause
